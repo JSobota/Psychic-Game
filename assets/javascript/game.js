0fc7c20782f545e1bf6e2b64d502db44efe4bc13
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-05-11 18:10:12 -0400

    Final Turn In

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,5 @@
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var userGuesses = []
-
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -39,7 +37,7 @@ updateGuessesLeft();
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  
+
   guessedLetters.push(userGuess);
   updateGuessesLeft();
   updateGuessesSoFar();
